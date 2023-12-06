b02a5b37d6b950e8603e034f2d9161cffb04ff3f
artilleur <99259153+artilleur@users.noreply.github.com>
2023-12-06 16:08:20 +0100

    06122023

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//  import { StatusBar } from 'expo-status-bar';
+//  import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//  export default function App() {
+//    return (
+//      <View style={styles.container}>
+//        <Text>j'aime pas les cons</Text>
+//        <StatusBar style="auto" />
+//      </View>
+//    );
+//  }
+
+//  const styles = StyleSheet.create({
+//    container: {
+//      flex: 1,
+//      backgroundColor: '#fff',
+//      alignItems: 'center',
+//      justifyContent: 'center',
+//    },
+//  });
+import React from 'react';
+import {View, Image, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  stretch: {
+    width: 400,
+    height: 400,
+    resizeMode: 'stretch',
   },
 });
+
+const DisplayAnImageWithStyle = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.stretch}
+        source={require('./images/images.jpeg')}
+      />
+
+<Image
+        style={styles.stretch}
+        source={require('./images/image2.jpg')}
+      />
+
+    </View>
+  );
+};
+
+export default DisplayAnImageWithStyle;
